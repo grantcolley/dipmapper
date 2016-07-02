bd53ec82b9ebe5dfce440c053543e462d9d891c5
grantcolley <grantcolley@yahoo.com>
2016-07-02 22:39:57 +0100

    Add test project
    Add test project

---
+++ DevelopmentInProgress.DipMapper.Test/DipMapperTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevelopmentInProgress.DipMapper.Test
+{
+    [TestClass]
+    public class DipMapperTest
+    {
+        [TestMethod]
+        public void GetFieldsTest()
+        {
+            // Arrange
+
+            // Act
+            string fields = DipMapper.GetSelectFields<TestDapperClass>();
+
+            // Assert
+            Assert.AreEqual(fields, "Id, Name, Date");
+        }
+    }
+}
---
+++ DevelopmentInProgress.DipMapper.Test/TestDapperClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DevelopmentInProgress.DipMapper.Test
+{
+    public class TestDapperClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public IEnumerable<TestDapperClass> TestDataClasses { get; set; }
+    }
+}
