6adb6a76f9923d39cd1d9c3335acb1edd5311e63
grantcolley <grantcolley@yahoo.com>
2016-07-05 14:34:47 +0100

    Test class changes
    Test class changes

---
+++ DevelopmentInProgress.DipMapper.Test/Activity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DevelopmentInProgress.DipMapper.Test
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Number { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime? NullableDate { get; set; }
+        public IEnumerable<Activity> Activities_1 { get; set; }
+        public IList<Activity> Activities_2 { get; set; }
+    }
+}
--- DevelopmentInProgress.DipMapper.Test/DipMapperTest.cs
+++ DevelopmentInProgress.DipMapper.Test/DipMapperTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DevelopmentInProgress.DipMapper.Test
 {
@@ -6,15 +7,58 @@ namespace DevelopmentInProgress.DipMapper.Test
     public class DipMapperTest
     {
         [TestMethod]
-        public void GetFieldsTest()
+        public void DipMapper_GetSelectSql_Test()
         {
             // Arrange
 
             // Act
-            string fields = DipMapper.GetSelectFields<TestDapperClass>();
+            string fields = DipMapper.GetSelectSql<Activity>();
 
             // Assert
-            Assert.AreEqual(fields, "Id, Name, Date");
+            Assert.AreEqual(fields, "SELECT Id, Name, Number, Date, NullableDate FROM Activity");
+        }
+
+        [TestMethod]
+        public void DipMapper_GetWhereSql_NullParameters_Test()
+        {
+            // Arrange
+
+            // Act
+            string where = DipMapper.GetWhereSql(null);
+
+            // Assert
+            Assert.AreEqual(where, "");
+        }
+
+        [TestMethod]
+        public void DipMapper_GetWhereSql_NoParameters_Test()
+        {
+            // Arrange
+
+            // Act
+            string where = DipMapper.GetWhereSql(new Dictionary<string, object>());
+
+            // Assert
+            Assert.AreEqual(where, "");
+        }
+
+        [TestMethod]
+        public void DipMapper_GetWhereSql_HasParameters_Test()
+        {
+            // Arrange
+            var activity = new Activity() {Id = 5, Name = "TestActivity"};
+            
+            var parameters = new Dictionary<string, object>();
+            parameters.Add("Id", activity.Id);
+            parameters.Add("Name", activity.Name);
+            parameters.Add("Date", activity.Date);
+            parameters.Add("NullableDate", activity.NullableDate);
+
+            // Act
+            string where = DipMapper.GetWhereSql(parameters);
+
+            // Assert
+            Assert.AreEqual(where, "WHERE Id=5 AND Name='TestActivity' AND Date='01/01/0001 00:00:00' AND NullableDate=null");
         }
     }
 }
