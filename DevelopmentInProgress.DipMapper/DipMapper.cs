b6b32d230b7bd934998ad8e1e300220e051e91d4
grantcolley <grantcolley@yahoo.com>
2016-07-08 21:39:33 +0100

    Dev in progress
    Dev in progress

--- DevelopmentInProgress.DipMapper/DipMapper.cs
+++ DevelopmentInProgress.DipMapper/DipMapper.cs
@@ -4,88 +4,134 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.Remoting.Messaging;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("DevelopmentInProgress.DipMapper.Test")]
 
 namespace DevelopmentInProgress.DipMapper
 {
     public static class DipMapper
     {
-        public static T Single<T>(this IDbConnection conn, Dictionary<string, object> paramaters = null, CommandType commandType = CommandType.TableDirect, string storedProcedure = "")
+        public static T Single<T>(this IDbConnection conn, Dictionary<string, object> paramaters = null)
         {
-            var target = Activator.CreateInstance<T>();
+            return Select<T>(conn, paramaters).FirstOrDefault();
+        }
 
-            var sql = GetCommandText<T>(commandType, paramaters);
-
+        public static IEnumerable<T> Select<T>(this IDbConnection conn, Dictionary<string, object> paramaters = null)
+        {
+            var sql = GetSqlSelect<T>() + GetSqlWhereClause(paramaters);
             OpenConnection(conn);
-
-            return target;
-        }
-
-        public static IEnumerable<T> Select<T>(this IDbConnection conn)
-        {
-            OpenConnection(conn);
-
-            var results = new List<T>();
-
-            return results;
-        }
-
-        public static string Insert<T>(this IDbConnection conn)
-        {
-            OpenConnection(conn);
-
-
-        }
-
-        public static void OpenConnection(IDbConnection conn)
-        {
-            if (conn.State == ConnectionState.Closed)
-            {
-                conn.Open();
-            }
-            else if (conn.State != ConnectionState.Open)
-            {
-                throw new Exception("Unable to open connection. ConnectionState=" + conn.State);
-            }
-        }
-
-        public static string GetCommandText<T>(CommandType commandType, Dictionary<string, object> paramaters)
-        {
-            if (commandType == CommandType.StoredProcedure)
-            {
-                return GetStoredProcedureParameters(paramaters);
-            }
-
-            return GetSelectSql<T>() + GetWhereSql(paramaters);
-        }
-
-        private static string GetStoredProcedureParameters(Dictionary<string, object> paramaters)
-        {
             throw new NotImplementedException();
         }
 
-        public static string GetSelectSql<T>()
+        public static T Insert<T>(this IDbConnection conn, T target, string IdentityField)
         {
-            string fields = GetFields<T>();
+            OpenConnection(conn);
+            throw new NotImplementedException();
+        }
 
+        public static T Update<T>(this IDbConnection conn, T target, Dictionary<string, object> paramaters = null)
+        {
+            var sql = GetSqlUpdate<T>(target, paramaters);
+            OpenConnection(conn);
+            throw new NotImplementedException();
+        }
+
+        public static void Delete<T>(this IDbConnection conn, Dictionary<string, object> paramaters = null)
+        {
+            var sql = GetSqlDelete<T>(paramaters);
+            OpenConnection(conn);
+            throw new NotImplementedException();
+        }
+
+        public static T ExecuteScalar<T>(this IDbConnection conn, Dictionary<string, object> paramaters = null, CommandType commandType = CommandType.TableDirect, string sql = "")
+        {
+            OpenConnection(conn);
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<T> Execute<T>(this IDbConnection conn, Dictionary<string, object> paramaters = null, CommandType commandType = CommandType.TableDirect, string sql = "")
+        {
+            OpenConnection(conn);
+            throw new NotImplementedException();
+        }
+
+        internal static string GetSqlSelect<T>()
+        {
+            return "SELECT " + GetFields<T>() + " FROM " + GetTableName<T>();
+        }
+
+        internal static string GetSqlUpdate<T>(T target, Dictionary<string, object> parameters)
+        {
+            IEnumerable<string> ignore = null;
+
+            if (parameters != null)
+            {
+                ignore = parameters.Keys;
+            }
+
+            return "UPDATE " + GetTableName<T>() + " SET " 
+                + GetFields<T>(target, ignore) + GetSqlWhereClause(parameters);
+        }
+
+        internal static string GetSqlDelete<T>(Dictionary<string, object> parameters)
+        {
+            return "DELETE FROM " + GetTableName<T>() + GetSqlWhereClause(parameters);
+        }
+
+        internal static string GetSqlWhereClause(Dictionary<string, object> parameters)
+        {
+            if (parameters == null
+                || !parameters.Any())
+            {
+                return string.Empty;
+            }
+
+            string where = " WHERE ";
+
+            foreach (var parameter in parameters)
+            {
+                where += parameter.Key + GetSqlWhereAssignment(parameter.Value) + " AND ";
+            }
+
+            if (where.EndsWith(" AND "))
+            {
+                where = where.Remove(where.Length - 5, 5);
+            }
+
+            return where;
+        }
+
+        internal static string GetTableName<T>()
+        {
             if (typeof (T).IsGenericType
                 && typeof (T).GenericTypeArguments.Any())
             {
-                return "SELECT " + fields + " FROM " + typeof (T).GenericTypeArguments[0].Name;
+                return typeof (T).GenericTypeArguments[0].Name;
             }
 
-            return "SELECT " + fields + " FROM " + typeof (T).Name;
+            return typeof (T).Name;
         }
 
-        private static string GetFields<T>(IEnumerable<string> ignore = null)
+        internal static string GetFields<T>(T target = default(T), IEnumerable<string> ignore = null)
         {
+            bool isUpdate = target != null;
             string fields = string.Empty;
+            PropertyInfo[] propertyInfos;
 
-            var propertyInfos = typeof(T).GetProperties();
+            if (isUpdate)
+            {
+                propertyInfos = target.GetType().GetProperties(BindingFlags.Public);
+            }
+            else
+            {
+                propertyInfos = typeof(T).GetProperties(BindingFlags.Public);
+            }
 
             foreach (var propertyInfo in propertyInfos)
             {
-                if (ignore != null
+                if (isUpdate
+                    && ignore != null
                     && ignore.Contains(propertyInfo.Name))
                 {
                     continue;
@@ -109,7 +155,14 @@ namespace DevelopmentInProgress.DipMapper
                     continue;
                 }
 
-                fields += propertyInfo.Name + ", ";
+                if (isUpdate)
+                {
+                    fields += propertyInfo.Name + GetSqlUpdateAssignment(propertyInfo.GetValue(target)) + ", ";
+                }
+                else
+                {
+                    fields += propertyInfo.Name + ", ";
+                }
             }
 
             if (fields.EndsWith(", "))
@@ -120,30 +173,7 @@ namespace DevelopmentInProgress.DipMapper
             return fields;
         }
 
-        public static string GetWhereSql(Dictionary<string, object> parameters)
-        {
-            if (parameters == null
-                || !parameters.Any())
-            {
-                return string.Empty;
-            }
-
-            string where = "WHERE ";
-
-            foreach (var parameter in parameters)
-            {
-                where += parameter.Key + WhereValue(parameter.Value) + " AND ";
-            }
-
-            if (where.EndsWith(" AND "))
-            {
-                where = where.Remove(where.Length - 5, 5);
-            }
-
-            return where;
-        }
-
-        private static string WhereValue(object value)
+        internal static string GetSqlWhereAssignment(object value)
         {
             var result = SqlConvert(value);
             if (result == "null")
@@ -154,7 +184,12 @@ namespace DevelopmentInProgress.DipMapper
             return "=" + result;
         }
 
-        private static string SqlConvert(object value)
+        internal static string GetSqlUpdateAssignment(object value)
+        {
+            return "=" + SqlConvert(value);
+        }
+
+        internal static string SqlConvert(object value)
         {
             if (value == null)
             {
@@ -186,5 +221,32 @@ namespace DevelopmentInProgress.DipMapper
                     return value.ToString();
             }
         }
+
+        private static void OpenConnection(IDbConnection conn)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            else if (conn.State != ConnectionState.Open)
+            {
+                throw new Exception("Unable to open connection. ConnectionState=" + conn.State);
+            }
+        }
+
+        //private static string GetCommandText<T>(CommandType commandType, Dictionary<string, object> paramaters)
+        //{
+        //    if (commandType == CommandType.StoredProcedure)
+        //    {
+        //        return GetStoredProcedureParameters(paramaters);
+        //    }
+
+        //    return GetSelectSql<T>() + GetWhereSql(paramaters);
+        //}
+
+        //private static string GetStoredProcedureParameters(Dictionary<string, object> paramaters)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
