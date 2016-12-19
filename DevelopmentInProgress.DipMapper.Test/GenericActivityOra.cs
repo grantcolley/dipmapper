6b03911005b7c3db8c76f0a850fb7f09d544a391
grantcolley <grantcolley@yahoo.com>
2016-12-19 20:55:32 +0000

    Add test classes for Oracle test cases.
    Add test classes for Oracle test cases.

---
+++ DevelopmentInProgress.DipMapper.Test/GenericActivityOra.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DevelopmentInProgress.DipMapper.Test
+{
+    public class GenericActivityOra<T>
+    {
+        public GenericActivityOra()
+        {
+            Activities_1 = new List<GenericActivityOra<T>>();
+            Activities_2 = new List<GenericActivityOra<T>>();
+            GroupIds = new T[3];
+            Description = "Desc...";
+            AssociatedActivityId = 3;
+            ParentActivityId = 5;
+        }
+
+        // Supported by DipMaper
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Status { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public ActivityTypeEnum ActivityType { get; set; }
+
+        // Supported by DipMapper if T is a value type.
+        public T GenericProperty { get; set; }
+
+        // Not supported by DipMapper
+        public GenericActivityOra<T> ParentActivity { get; set; }
+        public IEnumerable<GenericActivityOra<T>> Activities_1 { get; set; }
+        public IList<GenericActivityOra<T>> Activities_2 { get; set; }
+        public T[] GroupIds { get; set; }
+        internal string Description { get; set; }
+        protected int AssociatedActivityId { get; set; }
+        private int ParentActivityId { get; set; }
+    }
+}
